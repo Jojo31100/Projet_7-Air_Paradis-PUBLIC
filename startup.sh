2beba4715c93434799d5fab107597303ab09609c
Jojo31100 <Joel@Caquelin.Fr>
2025-09-01 14:36:31 +0200

    Probleme de demarrage - Mode DEBUG

--- startup.sh
+++ startup.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 #Active le VirtualEnv créé par Oryx
-source antenv/bin/activate
+if [ -d "antenv/bin" ]; then
+  source antenv/bin/activate
+elif [ -d "/home/site/wwwroot/antenv/bin" ]; then
+  source /home/site/wwwroot/antenv/bin/activate
+elif [ -d "/tmp/8dde94fb1e7b0e7/antenv/bin" ]; then
+  source /tmp/8dde94fb1e7b0e7/antenv/bin/activate
+fi
 
 #Lance FastAPI via uvicorn
-exec uvicorn api:app --host 0.0.0.0 --port ${PORT:-8000}
+exec uvicorn api:app --host 0.0.0.0 --port ${PORT:-8000} --reload --log-level debug
