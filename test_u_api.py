a62534d13b14cdf80d61915266d9a80b5d743fb4
Jojo31100 <Joel@Caquelin.Fr>
2025-09-03 17:13:14 +0200

    Mise en production API v1.0 : tests unitaires - nième patch sur le mock Azure Storage...

--- test_u_api.py
+++ test_u_api.py
@@ -5,23 +5,25 @@ import unittest.mock as mock
 from fastapi.testclient import TestClient
 import numpy
 
+
 #Patch de la variable d'env (pour éviter le plantage lors des tests U)
 os.environ["AZURE_STORAGE_CONNECTION_STRING"] = ("DefaultEndpointsProtocol=https;AccountName=fake;AccountKey=fake;EndpointSuffix=core.windows.net")
 
+
 #On patch le service client Azure en premier pour qu'il soit actif AVANT l'importation de l'API !
-with mock.patch("api.BlobServiceClient.from_connection_string") as mock_blob_service_client:
-    #On simule le client de conteneur pour qu'il ne se connecte pas et ne télécharge rien
+with mock.patch("api.BlobServiceClient") as mock_blob_service_client_class:
+    #On simule le client de conteneur, en s'assurant qu'il retourne une liste vide de blobs
     mock_container_client = mock.MagicMock()
     mock_container_client.get_container_client.return_value.list_blobs.return_value = []
     #On configure le BlobServiceClient pour qu'il retourne notre conteneur mock simulé
-    mock_blob_service_client.return_value = mock_container_client
+    mock_blob_service_client_class.from_connection_string.return_value = mock_container_client
 
     #On simule les deux chargements de modèles
     with mock.patch("api.tensorflow.keras.models.load_model") as mock_keras_load_model:
         with mock.patch("api.tensorflow_hub.load") as mock_hub_load:
 
             #Etape 1 : on simule l'encodeur USE,
-            #qui va renvoyer un "embedding" factice
+            # qui va renvoyer un "embedding" factice
             mock_encoder = mock.MagicMock()
             mock_encoder.return_value = numpy.zeros(512)
             mock_hub_load.return_value = mock_encoder
@@ -30,7 +32,7 @@ with mock.patch("api.BlobServiceClient.from_connection_string") as mock_blob_ser
             mock_model = mock.MagicMock()
 
             #Etape 3 : on configure la méthode .predict() du mock pour qu'elle
-            #renvoie un résultat spécifique en fonction du texte d'entrée
+            # renvoie un résultat spécifique en fonction du texte d'entrée
             def mock_predict_keras(embedding):
                 #Normalement, la prédiction devrait se faire sur l'embedding...
                 #Mais pour le test, nous savons que l'embedding est lié au tweet
